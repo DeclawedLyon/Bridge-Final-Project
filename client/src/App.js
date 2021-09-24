85802f4bb7c141618bbafba3eec52b8a75ca6063
vagrant <t.oleary@me.com>
2021-09-24 23:23:00 +0000

    add migrations with couriers

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,11 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: "Click the button to load data!",
-    };
-  }
+export default function App(props) {
+  const [state, setState] = useState("Click the button to load data!");
 
-  fetchData = () => {
+  const fetchData = () => {
     axios
       .get("/api/data") // You can simply make your requests to "/api/whatever you want"
       .then((response) => {
@@ -18,20 +13,14 @@ class App extends Component {
         console.log(response.data); // The entire response from the Rails API
 
         console.log(response.data.message); // Just the message
-        this.setState({
-          message: response.data.message,
-        });
+        setState(response.data.message);
       });
   };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>{this.state.message}</h1>
-        <button onClick={this.fetchData}>Fetch Data</button>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>{state}</h1>
+      <button onClick={() => fetchData()}>Fetch Data</button>
+    </div>
+  );
 }
-
-export default App;
