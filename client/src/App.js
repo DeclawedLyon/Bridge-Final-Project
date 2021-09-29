4240526f06a4f06aceb49366ea31e47c2266808b
vagrant <t.oleary@me.com>
2021-09-29 00:33:21 +0000

    stash

--- client/src/App.js
+++ client/src/App.js
@@ -12,40 +12,48 @@ export default function App(props) {
     currentUser: 1,
     currentUserObj: {},
     currentCourier: 1,
-    currentCourierObj: {}
+    currentCourierObj: {},
   });
 
   useEffect(() => {
     Promise.all([
       axios.get("/packages"),
       axios.get(`/api/users/${state.currentUser}`),
-      axios.get(`/api/couriers/${state.currentCourier}`)
+      axios.get(`/api/couriers/${state.currentCourier}`),
     ]).then((response) => {
       setState({
         packages: response[0].data,
         currentUser: response[1].data.user.id,
         currentUserObj: response[1].data,
         currentCourier: response[2].data.courier.id,
-        currentCourierObj: response[2].data
-      })
+        currentCourierObj: response[2].data,
+      });
     });
-  }, [])
+    // eslint-disable-next-line
+  }, []);
 
-  const mappedPackages = state.packages.map(mappedPackage => {
+  const mappedPackages = state.packages.map((mappedPackage) => {
     return (
       <TrackedPackage
-      key={`package-${mappedPackage.id}`}
-      nickname={mappedPackage.nickname === "N/A" ? mappedPackage.tracking_number : mappedPackage.nickname}
-      sender={mappedPackage.sent_from}
-      recipient={mappedPackage.sent_to}
-      logo={mappedPackage.courier}
-      statusMessage={mappedPackage.last_known_status}
-      delivered={mappedPackage.last_known_status === "DE" ? true : false}
-      delayed={mappedPackage.last_known_status === "EX" ? true : false}
-      enRoute={mappedPackage.last_known_status === "OF" ? true : false}
+        key={`package-${mappedPackage.id}`}
+        nickname={
+          mappedPackage.nickname === "N/A"
+            ? mappedPackage.tracking_number
+            : mappedPackage.nickname
+        }
+        sender={mappedPackage.sent_from}
+        recipient={mappedPackage.sent_to}
+        logo={mappedPackage.courier}
+        statusMessage={mappedPackage.last_known_status}
+        delivered={mappedPackage.last_known_status === "DE" ? true : false}
+        delayed={mappedPackage.last_known_status === "EX" ? true : false}
+        enRoute={mappedPackage.last_known_status === "OF" ? true : false}
       />
-    )
-  })
+    );
+  });
+
+  const [selectedPackage, setSelectedPackage] = useState(state.packages[0]);
+  setSelectedPackage("***:", state.packages[0]);
 
   return (
     <div className="App">
@@ -53,7 +61,12 @@ export default function App(props) {
         <h1>{state.currentUser}</h1>
         <h1>{state.currentCourier}</h1>
         <PackageManager />
-        <Viewer />
+        <Viewer
+          trkNum={selectedPackage.tracking_number}
+          description={selectedPackage.description}
+          sentTo={selectedPackage.sent_city_province}
+          sentFrom={selectedPackage.from_city_province}
+        />
         <Counters />
       </section>
 
--- client/src/components/Viewer.js
+++ client/src/components/Viewer.js
@@ -1,10 +1,22 @@
 import React from "react";
-import './Viewer.css';
+import "./Viewer.css";
 
-export default function Viewer() {
- return (
-  <main className="viewer">
-    <p>I'm the viewer component!</p>
-  </main>
- ) ;
+export default function Viewer(props) {
+  return (
+    <main className="viewer">
+      <p>I'm the viewer component!</p>
+      <div>
+        <p>{props.trkNum}</p>
+      </div>
+      <div>
+        <p>{props.description}</p>
+      </div>
+      <div>
+        <p>{props.sentTo}</p>
+      </div>
+      <div>
+        <p>{props.sentFrom}</p>
+      </div>
+    </main>
+  );
 }
